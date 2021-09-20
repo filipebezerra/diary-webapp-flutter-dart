85952bdf3f9493e371861cf5045782b445872c91
Filipe Bezerra <filipebzerra@gmail.com>
2021-09-20 06:45:38 -0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/diary_webapp_flutter_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diary_webapp_flutter_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diary Book',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        toolbarHeight: 100,
+        elevation: 4,
+      ),
+    );
+  }
+}
